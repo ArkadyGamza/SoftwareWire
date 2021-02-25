9f835eb8202e511e0722c345c81a02d2774451b8
Arkadiy Gamza <arkadiygamza@New-note.local>
2021-02-25 21:25:27 +0000

    Implement empty functions to make linker happy

--- SoftwareWire.h
+++ SoftwareWire.h
@@ -27,8 +27,8 @@ public:
   void begin();
 
   // Generate compile error when slave mode begin(address) is used
-  void __attribute__ ((error("I2C/TWI Slave mode is not supported by the SoftwareWire library"))) begin(uint8_t addr);
-  void __attribute__ ((error("I2C/TWI Slave mode is not supported by the SoftwareWire library"))) begin(int addr);
+  void __attribute__ ((error("I2C/TWI Slave mode is not supported by the SoftwareWire library"))) begin(uint8_t addr){};
+  void __attribute__ ((error("I2C/TWI Slave mode is not supported by the SoftwareWire library"))) begin(int addr){};
 
   void setClock(uint32_t clock);
   void beginTransmission(uint8_t address);
